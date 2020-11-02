2d3c4ad8524ec9c361b53ed19db71f639d7279d6
Hassan El Mghari <he55@drexel.edu>
2020-11-01 19:30:20 -0500

    cards done and data fetched from api

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import Chart from "./components/Chart/Chart";
 import Cards from "./components/Cards/Cards";
 import CountryPicker from "./components/CountryPicker/CountryPicker";
 import styles from "./App.module.css";
+import { fetchData } from "./api";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState();
+
+  async function getData() {
+    const fetchedData = await fetchData();
+    setData(fetchedData);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className={styles.container}>
-      <Cards />
-      <CountryPicker />
-      <Chart />
+      <Cards data={data} />
+      {/* <CountryPicker />
+      <Chart /> */}
     </div>
   );
 }
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,61 @@
 import React from "react";
+import styles from "./Cards.module.css";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import CountUp from "react-countup";
+import cx from "classnames";
 
-function Cards() {
+// TODO: Troubleshoot why double destructuring of props didn't work here
+// data: { confirmed, recovered, deaths, lastUpdate }
+function Cards({ data }) {
+  if (!data) {
+    return "Loading...";
+  }
   return (
-    <div>
-      <h1>Cards</h1>
+    <div className={styles.container}>
+      <Grid container spacing={3} justify-="center">
+        <Grid item component={Card} className={cx(styles.card, styles.infected)}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">
+              <CountUp start={0} end={data.confirmed.value} duration={2.5} separator="," />
+            </Typography>
+            <Typography color="textSecondary" gutterBottom>
+              {new Date(data.lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2">Number of COVID-19 cases</Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Recovered
+            </Typography>
+            <Typography variant="h5">
+              <CountUp start={0} end={data.recovered.value} duration={2.5} separator="," />
+            </Typography>
+            <Typography color="textSecondary" gutterBottom>
+              {new Date(data.lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2">Number of COVID-19 recoveries</Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
+            <Typography variant="h5">
+              <CountUp start={0} end={data.deaths.value} duration={2.5} separator="," />
+            </Typography>
+            <Typography color="textSecondary" gutterBottom>
+              {new Date(data.lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2">Number of COVID-19 deaths</Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
     </div>
   );
 }
